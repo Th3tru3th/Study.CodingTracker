649fb2e55bba7e1d6f1fd3bb7cbb4c49847f7a52
gabe <gabrielblunt@gmail.com>
2025-02-05 17:44:29 -0500

    Console.WriteLine modified

--- Coding Tracker/Program.cs
+++ Coding Tracker/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//For notes
+Console.WriteLine("Hello, C# Academy. I hope I can learn this!");
